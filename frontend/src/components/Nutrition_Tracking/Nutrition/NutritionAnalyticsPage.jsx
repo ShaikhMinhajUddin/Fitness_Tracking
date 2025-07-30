2e54172eb84933c67a95b04e02abf8269662b5e3
Shaikh Minhaj Uddin <149568357+ShaikhMinhajUddin@users.noreply.github.com>
2025-07-30 16:18:16 +0500

    Update NutritionAnalyticsPage.jsx

--- frontend/src/components/Nutrition_Tracking/Nutrition/NutritionAnalyticsPage.jsx
+++ frontend/src/components/Nutrition_Tracking/Nutrition/NutritionAnalyticsPage.jsx
@@ -14,7 +14,7 @@ const NutritionAnalyticsPage = () => {
     const fetchData = async () => {
       try {
         const token = localStorage.getItem('token'); 
-        const response = await axios.get('http://localhost:3000/nutrition/',{
+        const response = await axios.get('https://fitness-tracker-production-ba8c.up.railway.app/nutrition/',{
           headers: {
             Authorization: `Bearer ${token}` 
           }
