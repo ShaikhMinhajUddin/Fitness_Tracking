fdf91b2c852b0d4685c6eb8b69979f77e4075e64
Shaikh Minhaj Uddin <149568357+ShaikhMinhajUddin@users.noreply.github.com>
2025-07-30 16:12:55 +0500

    Update ProfilePage.jsx

--- frontend/src/components/User_Account/ProfilePage.jsx
+++ frontend/src/components/User_Account/ProfilePage.jsx
@@ -30,7 +30,7 @@ const ProfilePage = () => {
     };
 
     const imageSrc = user?.profileImage
-        ? `http://localhost:3000/${user.profileImage}`
+        ? `https://fitness-tracker-production-ba8c.up.railway.app/${user.profileImage}`
          : `https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg`;
 
     return (
